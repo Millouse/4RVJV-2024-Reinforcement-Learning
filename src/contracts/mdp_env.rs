07e4232478738b1e7d00e4564f8a1fd97677b66c
comeprz <come.przybyszewski@gmail.com>
2024-11-17 14:11:42 +0100

    Revert "Gridworld"

--- src/contracts/mdp_env.rs
+++
@@ -1,7 +0,0 @@
-pub trait MDPEnv {
-    fn num_states() -> usize;
-    fn num_actions() -> usize;
-    fn num_rewards() -> usize;
-    fn reward(index: usize) -> f32;
-    fn transition_probability(state: usize, action: usize, next_state: usize, reward_index: usize) -> f32;
-}
--- src/envs/secret/paths.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(target_os = "linux")]
-pub const SECRET_ENV_PATH: &str = "./libs/libsecret_envs.so";
-#[cfg(all(target_os = "macos", target_arch = "x86_64"))]
-pub const SECRET_ENV_PATH: &str = "./libs/libsecret_envs_intel_macos.dylib";
-#[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-pub const SECRET_ENV_PATH: &str = "./libs/libsecret_envs.dylib";
-#[cfg(windows)]
-pub const SECRET_ENV_PATH: &str = "./libs/secret_envs.dll";
